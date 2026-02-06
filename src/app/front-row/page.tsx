2f50f77ba9a2e2d770789faf1ec597c54cd82e45
Sarthak Singh <sarthak9singh@gmail.com>
2026-02-06 13:36:32 +0530

    feat: add front row page files

---
+++ src/app/front-row/page.tsx
@@ -0,0 +1,9 @@
+import CTASection from "./_components/CTASection";
+
+export default function FrontRow() {
+  return (
+    <div className="flex flex-col gap-8 lg:gap-16">
+      <CTASection />
+    </div>
+  );
+}
